141ad826ce6fe6c4fe968f2f86093ae0e1e6db68
Ismail Cam <i@bigstep.dk>
2017-11-16 09:51:55 +0100

    Build jar artifact to running in command-line

--- src/example/Serve.java
+++ src/example/Serve.java
@@ -8,9 +8,12 @@ public class Serve
 {
     public static void main( String[] args )
     {
+        System.out.println("Starting server...");
         Object implementor = new BankController();
         String address = "http://localhost:9000/rulebase";
         Endpoint.publish( address, implementor );
+        System.out.println("The server is now running. (press CTRL+C to exit the server)");
+        System.out.println("Address: " + address);
     }
 }
 
