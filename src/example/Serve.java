954362350b51e764f4e44f09feeeb87e66f83ab7
Ismail Cam <i@bigstep.dk>
2017-11-16 10:13:58 +0100

    Rebuild jar artifact

--- src/example/Serve.java
+++ src/example/Serve.java
@@ -8,11 +8,11 @@ public class Serve
 {
     public static void main( String[] args )
     {
-        System.out.println("Starting server...");
+        System.out.println("Starting Rule Base service...");
         Object implementor = new BankController();
         String address = "http://localhost:9000/rulebase";
         Endpoint.publish( address, implementor );
-        System.out.println("The server is now running. (press CTRL+C to exit the server)");
+        System.out.println("Service is now running. (press CTRL+C to exit the server)");
         System.out.println("Address: " + address);
     }
 }
